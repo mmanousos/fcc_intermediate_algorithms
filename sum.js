53d6ea1804b9b5902bd2486393e6c9046f43700f
M. Manousos <mmanousos@gmail.com>
2017-12-07 09:59:55 -0800

    assignment

---
+++ sum.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+/*-- ASSIGNMENT --*/
+
+/* Sum All Numbers in a Range
+We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+The lowest number will not always come first.
+
+Here are some helpful links:
+
+Math.max()
+Math.min()
+Array.prototype.reduce()
+
+
+CHECKS: 
+sumAll([1, 4]) should return a number.
+sumAll([1, 4]) should return 10.
+sumAll([4, 1]) should return 10.
+sumAll([5, 10]) should return 45.
+sumAll([10, 5]) should return 45.
+*/ 
+
