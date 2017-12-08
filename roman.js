040baaa6c91f6fcf0b6caff3ec28d782b75459ca
M. Manousos <mmanousos@gmail.com>
2017-12-08 10:14:20 -0800

    functional for two digits with pseudocode and assignment

--- roman.js
+++ roman.js
@@ -57,3 +57,80 @@ checkDigit();
 };
 
 convertToRoman(36);
+
+
+
+/*-- PSEUDOCODE --*/
+
+/* 
+hard code values 
+check how many digits are in the given parameter. 
+    use CharAt to check each digit? 
+Detailed if / switch function: 
+    check if it is below 4, = 4, = 5, above 5, = 9
+        below 4, get number and input that # of I; above 5, get (number - 5) and inpput that # of I after V. 
+        
+    if single digit    
+        return / break
+    if more digits, check digits individually. 
+        check if it is below 4, = 4, = 5, above 5, = 9
+            in 10s place, replace with X or L; in 100s place, with C or D; in 1000s place, with M.             
+    join results into single string. 
+    return string. 
+*/
+
+
+
+/*-- ASSIGNMENT --*/
+/*
+Roman Numeral Converter
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+
+Here are some helpful links:
+
+
+1 = I
+5 = V
+10 = X
+50 = L
+100 = C
+500 = D
+1000 = M
+
+4 = IV 
+40 = XL
+90 = XC
+
+Roman Numerals: http://www.mathsisfun.com/roman-numerals.html
+Array.prototype.splice(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+Array.prototype.indexOf(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+Array.prototype.join(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+convertToRoman(2) should return "II".
+convertToRoman(3) should return "III".
+convertToRoman(4) should return "IV".
+convertToRoman(5) should return "V".
+convertToRoman(9) should return "IX".
+convertToRoman(12) should return "XII".
+convertToRoman(16) should return "XVI".
+convertToRoman(29) should return "XXIX".
+convertToRoman(44) should return "XLIV".
+convertToRoman(45) should return "XLV"
+convertToRoman(68) should return "LXVIII"
+convertToRoman(83) should return "LXXXIII"
+convertToRoman(97) should return "XCVII"
+convertToRoman(99) should return "XCIX"
+convertToRoman(500) should return "D"
+convertToRoman(501) should return "DI"
+convertToRoman(649) should return "DCXLIX"
+convertToRoman(798) should return "DCCXCVIII"
+convertToRoman(891) should return "DCCCXCI"
+convertToRoman(1000) should return "M"
+convertToRoman(1004) should return "MIV"
+convertToRoman(1006) should return "MVI"
+convertToRoman(1023) should return "MXXIII"
+convertToRoman(2014) should return "MMXIV"
+convertToRoman(3999) should return "MMMCMXCIX"
+*/
