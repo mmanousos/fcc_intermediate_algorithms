ae2c82b0a309ae6c2ee94e28aad36a37d522da19
M. Manousos <mmanousos@gmail.com>
2017-12-12 12:36:38 -0800

    assignment

---
+++ missingletters.js
@@ -0,0 +1,16 @@
+
+/*-- ASSIGNMENT --*/
+/* Missing letters
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+Here are some helpful links:
+
+String.prototype.charCodeAt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+String.fromCharCode(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+
+fearNotLetter("abce") should return "d".
+fearNotLetter("abcdefghjklmno") should return "i".
+fearNotLetter("bcd") should return undefined.
+fearNotLetter("yz") should return undefined.
