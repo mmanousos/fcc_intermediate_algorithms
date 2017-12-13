462b163b7da5da369b71376ce6cf3cfbece710ff
M. Manousos <mmanousos@gmail.com>
2017-12-12 18:40:04 -0800

    several solution variations

--- boo.js
+++ boo.js
@@ -1,5 +1,59 @@
 
+/* correct solutions */ 
+
+1) 
+function booWho(bool) {
+  if (bool === typeof boolean) { 
+    return true;
+  } else {
+    return false; 
+  }
+}
+booWho(null);
+
+2) most easily read 
+function booWho(bool) {
+  if (typeof bool === 'boolean') { 
+    return true;
+  } else {
+    return false; 
+  }
+}
+booWho(null);
+
+3) more refined (but only works because the statement evaluates to the required output)
+function booWho(bool) {
+  return typeof bool === 'boolean'; 
+}
+
+booWho(null);
+
+
+
+/* very basic solution */
+/* doeesn't check type of argument */ 
+
+function booWho(bool) {
+  if ((bool === true) || (bool === false)) {
+    return true;
+  } else {
+    return false; 
+  } 
+}
+
+booWho(null);
+
+/*-- PSEUDOCODE --*/
+
+// value can only be true or false, not the strings 'true' or 'false' or any other 'truthy' or 'falsy' values. 
+
+// check if argument === true or false
+ // if yes, return true
+ // else return false
+
+
 /*-- ASSIGNMENT --*/
+
 /*
 Boo who
 Check if a value is classified as a boolean primitive. Return true or false.
