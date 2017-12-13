6630e9553f0077d65a419f75c81d442b6466e8b6
M. Manousos <mmanousos@gmail.com>
2017-12-12 17:14:52 -0800

    assignmennt

---
+++ boo.js
@@ -0,0 +1,23 @@
+
+/*-- ASSIGNMENT --*/
+/*
+Boo who
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Here are some helpful links:
+
+Boolean Objects: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+
+booWho(true) should return true.
+booWho(false) should return true.
+booWho([1, 2, 3]) should return false.
+booWho([].slice) should return false.
+booWho({ "a": 1 }) should return false.
+booWho(1) should return false.
+booWho(NaN) should return false.
+booWho("a") should return false.
+booWho("true") should return false.
+booWho("false") should return false.
+*/
