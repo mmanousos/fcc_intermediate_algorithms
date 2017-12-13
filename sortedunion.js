7492d918a246cdcad05df21f4bed809f0bd0f643
M. Manousos <mmanousos@gmail.com>
2017-12-12 19:03:40 -0800

    pseudocode

--- sortedunion.js
+++ sortedunion.js
@@ -1,4 +1,37 @@
 
+/*-- PSEUDOCODE --*/
+
+/* example of reduce() method
+ var largest = arr.reduce(function(a,b) {
+    return Math.max(a,b);
+ });
+*/
+
+/* more info about reduce() method 
+Returns a new array, does not modify the original source. 
+Applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value. Executes for each element in the array and an initial value (the accumulator “result/total/value”).
+E.g. var arr = [1, 2, 3, 4];
+var total = arr.reduce(function(sum, value) {
+  return sum + value;
+}, 0);
+console.log(total); // 10
+// Takes all the values in the original array and reduces them to a final, single value based on the logic that we pass in the callback function. 0 stands for the initial value of the sum.
+*/ 
+
+
+// create new empty array to return
+// cycle through arrays of argument
+// check if element exists in new array
+    // if no, push element array 
+
+// *is this even possible?*
+// pull all values into new array (cycle through all values in all arrays into single array)
+// remove any duplicates (reduce?)
+// push to new array (if reduce, inherently creates new array)
+// return
+
+
+
 /*-- ASSIGNMENT --*/
 
 Sorted Union
