799f01b22e6a01a0254f2d93821966aa30d137b2
M. Manousos <mmanousos@gmail.com>
2017-12-14 09:51:16 -0800

    assignment

---
+++ spinal.js
@@ -0,0 +1,18 @@
+
+
+/*-- ASSIGNMENT --*/
+
+Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+(Also known as 'kebab' case.)
+
+Here are some helpful links:
+
+RegExp: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+String.prototype.replace(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+spinalCase("This Is Spinal Tap") should return "this-is-spinal-tap".
+spinalCase("thisIsSpinalTap") should return "this-is-spinal-tap".
+spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
+spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
+spinalCase("AllThe-small Things") should return "all-the-small-things".
