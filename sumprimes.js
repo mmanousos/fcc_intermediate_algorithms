83c1686db96f0f63deb53ca0b70970d485db9141
M. Manousos <mmanousos@gmail.com>
2017-12-15 15:45:35 -0800

    assignment

---
+++ sumprimes.js
@@ -0,0 +1,17 @@
+
+/*-- ASSIGNMENT --*/
+
+Sum All Primes
+Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+The provided number may not be a prime.
+
+Here are some helpful links:
+For Loops: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+Array.prototype.push(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
+sumPrimes(10) should return a number.
+sumPrimes(10) should return 17.
+sumPrimes(977) should return 73156.
