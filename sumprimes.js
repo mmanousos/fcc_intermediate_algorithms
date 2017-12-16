fc704f64323820eb9afdaeb0bf877af615000def
M. Manousos <mmanousos@gmail.com>
2017-12-15 16:08:57 -0800

    pseudocode

--- sumprimes.js
+++ sumprimes.js
@@ -1,4 +1,27 @@
 
+/*-- PSEUDOCODE --*/
+
+// loop through numbers up to and including argument
+// check if number is prime
+    // can it be divided by 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+// if it is, add it to an array then reduce that array
+          // or simply add it to variable that will be returned after the loop ends. 
+
+
+/* Sieve of Eratosthenes (method for filtering out primes) */ 
+
+To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
+
+Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n). // create an array of all integers from 1 - argument
+Initially, let p equal 2, the smallest prime number. // set variable to 2
+Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked). // cycle through array and filter by p
+Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3. // check filtered array and get first value larger than p, reset to p & rerun until p = argument
+When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
+The main idea here is that every value given to p will be prime, because if it were composite it would be marked as a multiple of some other, smaller prime. Note that some of the numbers may be marked more than once (e.g., 15 will be marked both for 3 and 5).
+
+As a refinement, it is sufficient to mark the numbers in step 3 starting from p2, as all the smaller multiples of p will have already been marked at that point. This means that the algorithm is allowed to terminate in step 4 when p2 is greater than n.[1]
+
+
 /*-- ASSIGNMENT --*/
 
 Sum All Primes
