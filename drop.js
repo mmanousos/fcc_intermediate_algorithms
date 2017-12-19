082f5c34e2a7d47d74f26ceac3fe55d9283497ca
M. Manousos <mmanousos@gmail.com>
2017-12-19 11:49:25 -0800

    solution(s)

--- drop.js
+++ drop.js
@@ -1,6 +1,62 @@
 
+function dropElements(arr, func) {  
+ var filtered = arr.filter(func);
+ var first = arr[0];  
+ if (filtered[0] === first) {
+   return arr;
+ } else if (filtered == false) {
+   return filtered;
+ } else {
+   var num = arr.indexOf(filtered[0]);
+   var returnArray = arr.slice(num);
+   return returnArray;
+ }
+}
+  dropElements([1, 2, 3, 4], function(n) {return n > 5;});
+
+
+/* revised pseudocode that worked for my solution*/
+/* Logic is not very dissimilar to that used in the fcc intermediate solution (but does not use ES6 arrow functions or findIndex() function)*/ 
+
+// filter array by logic of arg func
+// set first value of arg arr to variable
+  // if first value of arg arr === to first value of filtered array, return entire array
+  // if filtered array is empty, return filtered array (empty array)
+  // otherwise, get first value of filtered array
+    // find it's position in the original array, slice from there & return
+
+
+
+
+
+/*fcc advanced solution is the cleanest version */
+
+function dropElements(arr, func) {
+  while(arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
+
+// test here
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
+
+// I forgot I could pass a new argument into the argument function which makes this solution a lot more simple.
+
 /*-- PSEUDOCODE --*/
 
+
+
+/* revised pseudocode */
+
+// create copy and check first digit against logic of function
+  // if true, return entire array
+  // if false, filter array by logic
+  // get first value of filtered array
+  // find it's position in the original array and slice from there
+
+
+/* original logic */
 // check first element of argument array against logic of argument function
     // if true, return full array
     // if not true, remove (shift) first & move on to next element to check
