7a7f6a57b64a44c2edb0e759db4bae060e67fa78
M. Manousos <mmanousos@gmail.com>
2017-12-20 16:43:33 -0800

    assignment

---
+++ binary.js
@@ -0,0 +1,14 @@
+
+
+/*-- ASSIGNMENT --*/
+
+Binary Agents
+Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
+
+Here are some helpful links:
+
+String.prototype.charCodeAt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+String.fromCharCode(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+
