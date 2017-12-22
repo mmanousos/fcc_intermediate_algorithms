8ae86e2f4946d4e06edc0860f567435b2f54018d
M. Manousos <mmanousos@gmail.com>
2017-12-21 17:26:42 -0800

    solution

--- binary.js
+++ binary.js
@@ -1,8 +1,25 @@
 
+function binaryAgent(str) {
+  var strNew = ""; 
+  var arr = str.split(' ');
+  
+  for (var i = 0; i < arr.length; i++) {
+   var val = String.fromCharCode(parseInt(arr[i], 2));
+   strNew += val;
+   } 
+  return strNew;
 
+}
+
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
 
 
 /*-- PSEUDOCODE --*/
+
+
+// use RegEx to find spaces in between blocks of characters ? 
+// convert string to an array breaking at spaces? 
 
 Looking over charts of binary characters, the spaces are built into the sample sentences. 
 // create empty string
